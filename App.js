5edf685bb4cb88496eb0fe243a2fffbcba59a095
Sergio Trejo <sergiotrejordz@gmail.com>
2020-04-29 20:40:08 -0500

    all at once

---
+++ App.js
@@ -0,0 +1,42 @@
+  import React, { useState } from 'react';
+  import { View, StyleSheet, Button, FlatList } from 'react-native';
+
+  import GoalItem from './components/GoalItem';
+  import GoalInput from './components/GoalInput';
+
+  export default function App() {
+    const [courseGoals, setCourseGoals] = useState([]);
+    const [isAddMode, setIsAddMode] = useState(false);
+
+    const addGoalHandler = goalTitle => {
+      setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }
+      ]);
+      setIsAddMode(false);
+
+    };
+    const removeGoalHandler = goalId => {
+      setCourseGoals(currentGoals =>{
+        return currentGoals.filter((goal) => goal.id !== goalId);
+      });
+    };
+
+    const cancelGoalAditionHandler = () => {
+      setIsAddMode(false);
+    };
+
+    return (
+      <View style={styles.screen}>
+        <Button title="Add New Task" onPress={() => setIsAddMode(true)}/>
+        <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAditionHandler}/>
+        <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={itemData => 
+        <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
+      />
+    </View>
+  );
+}
+
+  const styles = StyleSheet.create({
+    screen: {
+      padding: 50
+    },
+  });
